0ac3fd9ec34a16c0bc4b8477502b415fad7562b1
raspiotr <raspiotr@gmail.com>
2023-09-20 00:02:17 +0200

    search data about cat

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
+const apiKey =
   'live_UOs61qgqDHBR896dKOCKKP3GVLfO1RqJbthIXAB6dUXQ5lbcXL65dI8OB1kfoBHc';
+//axios.defaults.headers.common['x-api-key'] = apiKey;
 
-import { fetchBreeds } from './js/cat-api.js';
+import {
+  fetchBreeds,
+  fetchCatByBreed,
+  fetchCatImageByBreed,
+} from './js/cat-api.js';
 
 const catsList = document.querySelector('.breed-select');
 
@@ -21,3 +26,28 @@ document.addEventListener(
     .then(cats => renderCatsList(cats))
     .catch(error => console.log(error))
 );
+
+catsList.addEventListener('change', function (event) {
+  const breedId = event.target.value;
+  console.log('Wybrana opcja:', breedId);
+
+  fetchCatByBreed(breedId)
+    .then(cat => {
+      console.log(cat.name);
+      console.log(cat.description);
+      console.log(cat.temperament);
+    })
+    .catch(error => console.log(error));
+
+  fetchCatImageByBreed(breedId)
+    .then(data => {
+      // if (data && data.length > 0) {
+      //   const imageUrl = data[0].url;
+      //   console.log('Losowe zdjęcie kota:', imageUrl);
+      // } else {
+      //   console.log('Nie znaleziono zdjęcia kota.');
+      // }
+      console.log(data[0].url);
+    })
+    .catch(error => console.log(error));
+});
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,14 +1,50 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] =
+const apiKey =
   'live_UOs61qgqDHBR896dKOCKKP3GVLfO1RqJbthIXAB6dUXQ5lbcXL65dI8OB1kfoBHc';
+//axios.defaults.headers.common['x-api-key'] = apiKey;
+
+const headers = {
+  'x-api-key': apiKey,
+};
+
+const requestOptions = {
+  method: 'GET',
+  headers: headers,
+};
 
 export function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds').then(response => {
+  return fetch(`https://api.thecatapi.com/v1/breeds`, requestOptions).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      //console.log(response.json());
+      return response.json();
+    }
+  );
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/breeds/${breedId}`,
+    requestOptions
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-    //console.log(response.json());
+    return response.json();
+  });
+}
+
+export function fetchCatImageByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+    requestOptions
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
     return response.json();
   });
 }
