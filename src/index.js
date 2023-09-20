a48c777f98c299212a9d664a3fe6aeb03c321795
raspiotr <raspiotr@gmail.com>
2023-09-20 12:06:18 +0200

    load picture and text to page

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import {
   fetchCatImageByBreed,
 } from './js/cat-api.js';
 
+const body = document.querySelector('body');
+body.style.fontFamily = 'Verdana';
+
 const catsList = document.querySelector('.breed-select');
 
 function renderCatsList(cats) {
@@ -20,6 +23,35 @@ function renderCatsList(cats) {
   catsList.innerHTML = markup;
 }
 
+const catInfoBox = document.querySelector('.cat-info');
+
+function createImgElement(src, alt, height) {
+  const imgElement = document.createElement('img');
+  imgElement.src = src;
+  imgElement.alt = alt;
+  imgElement.height = height;
+  catInfoBox.prepend(imgElement);
+}
+
+function createTextInfoElement(name, description, temperament) {
+  const divElement = document.createElement('div');
+  divElement.style.maxWidth = '600px';
+  const headingElement = document.createElement('h1');
+  const descriptionElement = document.createElement('p');
+  const temperamentElement = document.createElement('p');
+  headingElement.textContent = name;
+  descriptionElement.textContent = description;
+  temperamentElement.textContent = temperament;
+  const temperTitle = document.createElement('span');
+  temperTitle.textContent = 'Temperament: ';
+  temperTitle.style.fontWeight = 'bold';
+  temperamentElement.prepend(temperTitle);
+  divElement.append(headingElement);
+  divElement.append(descriptionElement);
+  divElement.append(temperamentElement);
+  catInfoBox.append(divElement);
+}
+
 document.addEventListener(
   'DOMContentLoaded',
   fetchBreeds()
@@ -27,26 +59,33 @@ document.addEventListener(
     .catch(error => console.log(error))
 );
 
-catsList.addEventListener('change', function (event) {
+catsList.addEventListener('change', event => {
+  catInfoBox.innerHTML = '';
+  catInfoBox.style.display = 'flex';
+  catInfoBox.style.gap = '30px';
   const breedId = event.target.value;
   console.log('Wybrana opcja:', breedId);
+  fetchCatImageByBreed(breedId)
+    .then(data => {
+      if (data && data.length > 0) {
+        const imageUrl = data[0].url;
+        const catName =
+          event.target.options[event.target.selectedIndex].textContent;
+        console.log('Losowe zdjęcie kota:', imageUrl);
+        console.log('Name:', catName);
+        createImgElement(imageUrl, catName, 300);
+      } else {
+        console.log('Nie znaleziono zdjęcia kota.');
+      }
+    })
+    .catch(error => console.log(error));
 
   fetchCatByBreed(breedId)
     .then(cat => {
       console.log(cat.name);
       console.log(cat.description);
       console.log(cat.temperament);
-    })
-    .catch(error => console.log(error));
-
-  fetchCatImageByBreed(breedId)
-    .then(data => {
-      if (data && data.length > 0) {
-        const imageUrl = data[0].url;
-        console.log('Losowe zdjęcie kota:', imageUrl);
-      } else {
-        console.log('Nie znaleziono zdjęcia kota.');
-      }
+      createTextInfoElement(cat.name, cat.description, cat.temperament);
     })
     .catch(error => console.log(error));
 });
